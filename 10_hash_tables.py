c39aad849f517125f085b1c0a0eb5b1f5dcccbd6
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2022-09-27 16:02:18 +0100

    Add files via upload

---
+++ 10_hash_tables.py
@@ -0,0 +1,10 @@
+voted = {}
+
+
+def check_voter(name):
+    if voted.get(name):
+        print("Kick them out")
+    else:
+        voted[name] = True
+        print("let them vote")
+
---
+++ 11_breadth_first_search.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+graph = {}
+graph["you"] = ["bob", "claire", "alice"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+def person_is_seller(name):
+    return name[-1] == 'm'
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+                print(person + " is a mango seller")
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    return False
+
+
+search("you")
+
