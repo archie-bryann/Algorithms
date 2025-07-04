94fa2a65eb90d0a622584c4e20ca83fb66c2a577
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-07-04 18:53:53 +0100

    updated

--- 9_quick_sort.py
+++ 9_quick_sort.py
@@ -1,3 +1,6 @@
+# Quick sort uses Divide and Conquer
+# D&C algorithms are recursive algorithms
+
 def qsort(arr):
     if len(arr) < 2:
         return arr
@@ -5,3 +8,5 @@ def qsort(arr):
     lesser = [i for i in arr[1:] if i <= pivot]
     greater = [i for i in arr[1:] if i > pivot]
     return qsort(lesser) + [pivot] + qsort(greater)
+
+print(qsort([3,5,2,1,4]))
