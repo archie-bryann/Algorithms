1269f35018d747ae98898304b4e504da8d86037a
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-07-06 11:59:40 +0100

    added two pointers and sliding window

--- 17_two_pointers.py
+++ 17_two_pointers.py
@@ -1,1 +1,19 @@
 # https://www.youtube.com/watch?v=cQ1Oz4ckceM
+
+def two_sum(numbers, target):
+    l = 0
+    r = len(numbers) - 1
+
+    while l < r:
+        cur_sum = numbers[l] + numbers[r]
+
+        if cur_sum > target:
+            r -= 1
+        elif cur_sum < target:
+            l += 1
+        else:
+            # return [numbers[l], numbers[r]]
+            return [l + 1, r + 1]
+    return []
+
+print(two_sum([1,2,4,5,6,7,8,9,10], 15))
--- 18_sliding_window.py
+++ 18_sliding_window.py
@@ -1,1 +1,26 @@
 # https://www.youtube.com/watch?v=jM2dhDPYMQM
+
+def find_subarrays_with_sum(arr, target):
+    result = []
+    left = 0
+    current_sum = 0
+
+    for right in range(len(arr)):
+        # Expand window by adding current element
+        current_sum += arr[right]
+
+        # Shrink window from left while sum is greater than target
+        while current_sum > target and left <= right:
+            current_sum -= arr[left]
+            left += 1
+
+        # If we found a valid subarray
+        if current_sum == target:
+            result.append(arr[left:right+1])
+            # Move left pointer to find the next possible subarray
+            current_sum -= arr[left]
+            left += 1
+
+    return result
+
+print(find_subarrays_with_sum([1, 7, 4, 3, 1, 2, 1, 5, 1], 7))
